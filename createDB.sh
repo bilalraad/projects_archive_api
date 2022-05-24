24dc2188995177401658bddf28fe3b20a7f4440f
Bilal Ra'ad <bilal52040@gmail.com>
2022-05-24 07:15:31 +0300

    fix createdb

--- createDB.sh
+++ createDB.sh
@@ -2,14 +2,15 @@
     echo "Note: password will be hidden when typing"
     read -sp PASSWDDB
 # replace "-" with "_" for database username
-MAINDB="project_archive_api"
+MAINDB="projects_archive_api"
+MAINUSER="computerAdmin"
 
 # If /root/.my.cnf exists then it won't ask for root password
 if [ -f /root/.my.cnf ]; then
 
     mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -e "CREATE USER ${MAINUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINUSER}'@'localhost';"
     mysql -e "FLUSH PRIVILEGES;"
 
 # If /root/.my.cnf doesn't exist then it'll ask for root password
@@ -18,7 +19,7 @@ else
     echo "Note: password will be hidden when typing"
     read -sp rootpasswd
     mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINUSER}'@'localhost';"
     mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 fi
