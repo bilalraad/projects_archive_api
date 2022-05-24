7f77323d834ee49d9850110af1c55987e4419949
Bilal Ra'ad <bilal52040@gmail.com>
2022-05-24 20:06:41 +0300

    db sh update 2

--- createDB.sh
+++ createDB.sh
@@ -11,9 +11,11 @@ echo "$RESULT" == "$MAINDB"
 if [ "$RESULT" != "$MAINDB" ]; then
 mysql -u root -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 fi
+mysql -u root -p${rootpasswd} -e "select user,host from user';"
 mysql -u root -p${rootpasswd} -e "DROP USER ${MAINUSER}@localhost';"
 mysql -u root -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 mysql -u root -p${rootpasswd} -e "CREATE USER ${MAINUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
 mysql -u root -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINUSER}'@'localhost';"
+mysql -u root -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 
 
