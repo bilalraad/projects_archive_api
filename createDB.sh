2050b038f8204af0443734a6ae82c62f02ef8f1d
Bilal Ra'ad <bilal52040@gmail.com>
2022-05-24 19:41:37 +0300

    trying new way of creating db

--- createDB.sh
+++ createDB.sh
@@ -1,25 +1,17 @@
-    echo "Please enter root user MySQL password!"
-    echo "Note: password will be hidden when typing"
-    read -sp PASSWDDB
+echo "Please enter password of the root MySQL user!"
+echo "Note: password will be hidden when typing"
+read -sp PASSWDDB
 # replace "-" with "_" for database username
 MAINDB="projects_archive_api"
 MAINUSER="computerAdmin"
 
-# If /root/.my.cnf exists then it won't ask for root password
-if [ -f /root/.my.cnf ]; then
 
-    mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER ${MAINUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINUSER}'@'localhost';"
-    mysql -e "FLUSH PRIVILEGES;"
-
-# If /root/.my.cnf doesn't exist then it'll ask for root password
-else
-    echo "Please enter root user MySQL password!"
-    echo "Note: password will be hidden when typing"
-    read -sp rootpasswd
-    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINUSER}'@'localhost';"
-    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+RESULT=`mysqlshow --user=${MAINUSER} --password=${PASSWDDB} ${MAINDB}| grep -v Wildcard | grep -o ${MAINDB}`
+if [ "$RESULT" == ${MAINDB} ]; then
+mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 fi
+mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINUSER}'@'localhost';"
+mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+
+
