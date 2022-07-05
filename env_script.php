de18466d608f9aa919f0d8e9fff7e865ba6f9818
Bilal Raad <bilal52040@gmail.com>
2022-07-05 09:27:43 +0300

    fix: :art: env script for windows

--- env_script.php
+++ env_script.php
@@ -8,9 +8,9 @@ preg_match_all('~(["\'])([^"\']+)\1~', $env, $arr);
 // echo ($arr[0][0]);
 // echo $env;
 
-if(PHP_OS === 'WINNT'){
-    $str= shell_exec('ipconfig /all');
-}else{
+if (PHP_OS === 'WINNT') {
+    $str = shell_exec('ipconfig /all');
+} else {
     exec('ifconfig', $arr);
     $str = implode(";\n", $arr);
 }
@@ -19,20 +19,20 @@ if(PHP_OS === 'WINNT'){
 preg_match_all('/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/', $str, $ip_matches);
 // print_r($ip_matches);
 
-if(PHP_OS === 'WINNT'){
-    $ip = $ip_matches[0][2];
-}else{
+if (PHP_OS === 'WINNT') {
+    $ip = $ip_matches[0][0];
+} else {
 
     $ip = $ip_matches[0][1];
 }
 // print_r($ip);
 
-$baseurl = 'http://'. $ip . ':8000/' . 'api';
+$baseurl = 'http://' . $ip . ':8000/' . 'api';
 $newEnv = str_replace(array($arr[0][0]), "'" . $baseurl . "'", $env);
 echo $newEnv;
 file_put_contents('public/web/assets/assets/mobile.env', $newEnv);
 
-$runcommand = 'php artisan serve --host=' . $ip . " --port:8000";
+$runcommand = 'php artisan serve --host=' . $ip . " --port=8000";
 
 
 $runfile = file_get_contents('run.bat');
